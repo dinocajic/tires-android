7f83cf888862c5fce605e803b52792b56ab04118
dinocajic <dinocajic@gmail.com>
2019-07-11 11:18:02 -0400

    Added notes

--- src/main/java/edu/gsu/student/csc4360/Tire.java
+++ src/main/java/edu/gsu/student/csc4360/Tire.java
@@ -1,8 +1,5 @@
 package edu.gsu.student.csc4360;
 
-
-import android.util.Log;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
@@ -10,7 +7,7 @@ import java.util.regex.Pattern;
 public class Tire {
 
     private String brand, model, part_number, width, aspect_ratio, construction, wheel_diameter, max_load, max_psi,
-                  ply, load_rating, speed_rating, weight, cost, sales_price, cost_per_unit, image;
+                   ply, load_rating, speed_rating, weight, cost, sales_price, qty_per_unit, image;
 
     private boolean has_warranty, is_dot_approved, is_discontinued;
 
@@ -72,8 +69,8 @@ public class Tire {
         return sales_price;
     }
 
-    public String getCost_per_unit() {
-        return cost_per_unit;
+    public String getQty_per_unit() {
+        return qty_per_unit;
     }
 
     public String getImage() {
@@ -94,20 +91,27 @@ public class Tire {
 
     public boolean setBrand(String brand) {
         // todo get brands from Globals.db.getBrands() and verify that the brand actually exists
-        //      inside the returned array
+        //      inside the returned array. getBrands() returns an array of Brands
         this.brand = brand;
 
         return true;
     }
 
     public boolean setModel(String model) {
-        // todo get models from Globals.db.getBrands() and verify that the model actually exists
-        //      inside the returned array
+        // todo get models from Globals.db.getModels() and verify that the model actually exists
+        //      inside the returned array. getModels() returns an array of models
         this.model = model;
 
         return true;
     }
 
+    /**
+     * Checks to see that the number is between 0 and 20 characters, is alphanumeric, and sees if
+     * it's unique inside the tires table. If valid, sets it.
+     *
+     * @param part_number - Tire part number
+     * @return boolean
+     */
     public boolean setPart_number(String part_number) {
         if ( ! (part_number.length() > 0 && part_number.length() < 20) ) {
             return false;
@@ -117,11 +121,20 @@ public class Tire {
             return false;
         }
 
-        // TODO call partNumberExists(part_number) to check if the part number is unique or not
+        // TODO call Globals.db.partNumberExists(part_number) to check if the part number is unique or not
+
+        this.part_number = part_number;
 
         return true;
     }
 
+    /**
+     * Checks to see if the width submitted is a number and either 2 or 3 digits in length.
+     * If valid, sets it.
+     *
+     * @param width - Tire width
+     * @return boolean
+     */
     public boolean setWidth(String width) {
         try {
             int w = Integer.parseInt(width);
@@ -136,6 +149,12 @@ public class Tire {
         }
     }
 
+    /**
+     * Checks to see if the aspect ratio is exactly 2 digits in length. If valid, sets it.
+     *
+     * @param aspect_ratio - Tire Aspect Ratio
+     * @return boolean
+     */
     public boolean setAspect_ratio( String aspect_ratio ) {
         try {
             int w = Integer.parseInt(aspect_ratio);
@@ -152,6 +171,12 @@ public class Tire {
         }
     }
 
+    /**
+     * Checks to see that the internal construction is either R or ZR. If valid, sets it.
+     *
+     * @param construction - internal construction
+     * @return boolean
+     */
     public boolean setConstruction(String construction) {
         String[] constructionArray = new String[]{"R","ZR"};
         List<String> list = Arrays.asList(constructionArray);
@@ -165,6 +190,13 @@ public class Tire {
 
     }
 
+    /**
+     * Checks to see that the wheel diameter is numeric. If so, it then makes sure that the number
+     * of characters inside the wheel_diameter is either 2 or 4: i.e. 20 or 22.5. If valid, sets it.
+     *
+     * @param wheel_diameter - Wheel diameter
+     * @return boolean
+     */
     public boolean setWheel_diameter(String wheel_diameter) {
         double doubleDiam;
 
@@ -192,6 +224,13 @@ public class Tire {
 
     }
 
+    /**
+     * Checks to make sure that the max load is either 3 or 4 characters in length and numeric.
+     * If valid, sets it.
+     *
+     * @param max_load - Max Load
+     * @return boolean
+     */
     public boolean setMax_load(String max_load) {
        try {
            int ml = Integer.parseInt(max_load);
@@ -207,6 +246,14 @@ public class Tire {
        }
     }
 
+    /**
+     * Checks to make sure that the max psi is numeric, i smore than 1 digit in length, is either
+     * 2 or 3 digits in length if no period is present, and either 4 or 5 digits in length if a
+     * period is present. If valid, sets it.
+     *
+     * @param max_psi - Tire maximum PSI
+     * @return boolean
+     */
     public boolean setMax_psi(String max_psi) {
         if ( max_psi.length() <= 1 ) {
             return false;
@@ -228,12 +275,7 @@ public class Tire {
         String[] parameters = max_psi.split( Pattern.quote(".") );
 
         if (parameters.length == 2) {
-            if ( doublePSI > 0 && parameters[0].length() == 2 && parameters[1].length() == 1 ) {
-                this.max_psi = max_psi;
-                return true;
-            }
-
-            if ( doublePSI > 0 && parameters[0].length() == 3 && parameters[1].length() == 1 ) {
+            if ( doublePSI > 0 && (parameters[0].length() == 2 || parameters[0].length() == 3) && parameters[1].length() == 1 ) {
                 this.max_psi = max_psi;
                 return true;
             }
@@ -242,6 +284,12 @@ public class Tire {
         return false;
     }
 
+    /**
+     * Checks to see if the ply is a number between 0 and 12. If valid, sets it.
+     *
+     * @param ply - Tire Ply amount
+     * @return boolean
+     */
     public boolean setPly(String ply) {
         try {
             int p = Integer.parseInt(ply);
@@ -256,6 +304,12 @@ public class Tire {
         }
     }
 
+    /**
+     * Checks to make sure that the load rating matches one in the array. If valid, sets it.
+     *
+     * @param load_rating - Tire Load Rating
+     * @return boolean
+     */
     public boolean setLoad_rating(String load_rating) {
         String[] loadRating = new String[]{"B","C","D","E","F"};
         List<String> list = Arrays.asList(loadRating);
@@ -268,6 +322,12 @@ public class Tire {
         }
     }
 
+    /**
+     * Checks to make sure that the speed rating matches one in the array. If valid, sets it.
+     *
+     * @param speed_rating - Tire speed rating
+     * @return boolean
+     */
     public boolean setSpeed_rating(String speed_rating) {
         String[] speedRating = new String[]{"Z","P","S","R","Q","Y","V","W","T","H"};
         List<String> list = Arrays.asList(speedRating);
@@ -281,9 +341,15 @@ public class Tire {
 
     }
 
+    /**
+     * Checks to make sure that the weight is numeric, and if it does not have a period between 1 and
+     * 3 digits in length. If it contains a period, then it checks to make sure that the value is
+     * between 4 and 5 characters in length. If valid, sets it.
+     *
+     * @param weight - Tire weight
+     * @return boolean
+     */
     public boolean setWeight(String weight) {
-        this.weight = weight;
-
         double doubleWeight;
 
         try {
@@ -309,11 +375,17 @@ public class Tire {
         return false;
     }
 
+    /**
+     * Makes sure that the cost is numeric and greater than or equal to zero. If valid, sets it.
+     *
+     * @param cost - Cost of the tire
+     * @return boolean
+     */
     public boolean setCost(String cost) {
         try{
             double c = Double.parseDouble(cost);
 
-            if(c>0){
+            if(c>=0){
                 this.cost = cost;
                 return true;
             } else {
@@ -323,9 +395,14 @@ public class Tire {
         catch(NumberFormatException e) {
             return false;
         }
-
     }
 
+    /**
+     * Checks to make sure that the sales price is greater than zero and is numeric. If valid, sets it.
+     *
+     * @param sales_price - Selling price of the tire
+     * @return boolean
+     */
     public boolean setSales_price(String sales_price) {
         try{
            double ssp = Double.parseDouble(sales_price);
@@ -340,11 +417,17 @@ public class Tire {
         }
     }
 
-    public boolean setQty_per_unit(String cost_per_unit) {
+    /**
+     * Checks to see if the quantity per unit is numeric and is greater than or equal to 1. If valid, sets it.
+     *
+     * @param qty_per_unit - Quantity per unit
+     * @return boolean
+     */
+    public boolean setQty_per_unit(String qty_per_unit) {
         try {
-            int cpu = Integer.parseInt(cost_per_unit);
+            int cpu = Integer.parseInt(qty_per_unit);
             if(cpu>=1){
-                this.cost_per_unit = cost_per_unit;
+                this.qty_per_unit = qty_per_unit;
                 return true;
             }
             else {
@@ -356,6 +439,12 @@ public class Tire {
         }
     }
 
+    /**
+     * Checks to make sure that the length of the string is greater than 4. If valid, sets it.
+     *
+     * @param image - Image URI
+     * @return boolean
+     */
     public boolean setImage(String image) {
         if ( image.length() < 5 ) {
             return false;
@@ -365,14 +454,29 @@ public class Tire {
         return true;
     }
 
+    /**
+     * Sets has_warranty.
+     *
+     * @param has_warranty - Has Warranty
+     */
     public void setHas_warranty(boolean has_warranty) {
         this.has_warranty = has_warranty;
     }
 
+    /**
+     * Sets is_dot_approved
+     *
+     * @param is_dot_approved - Is DOT approved
+     */
     public void setIs_dot_approved(boolean is_dot_approved) {
         this.is_dot_approved = is_dot_approved;
     }
 
+    /**
+     * Sets is_discontinued;
+     *
+     * @param is_discontinued - Is discontinued?
+     */
     public void setIs_discontinued(boolean is_discontinued) {
         this.is_discontinued = is_discontinued;
     }
