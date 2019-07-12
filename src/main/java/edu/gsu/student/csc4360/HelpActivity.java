db5b651a435e09ff6be0ecd3102f2bbf0355ba38
dinocajic <dinocajic@gmail.com>
2019-07-12 15:13:34 -0400

    Removed testing

--- src/main/java/edu/gsu/student/csc4360/HelpActivity.java
+++ src/main/java/edu/gsu/student/csc4360/HelpActivity.java
@@ -3,6 +3,7 @@ package edu.gsu.student.csc4360;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class HelpActivity extends AppCompatActivity {
 
