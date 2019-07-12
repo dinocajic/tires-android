1f1cdaaf0846cf82a76aeae3097ea8d488d9fa91
dinocajic <dinocajic@gmail.com>
2019-07-12 15:12:09 -0400

    Added id

--- src/main/java/edu/gsu/student/csc4360/Brands.java
+++ src/main/java/edu/gsu/student/csc4360/Brands.java
@@ -21,6 +21,10 @@ public class Brands {
         return enabled;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
